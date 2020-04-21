b761b4b45bf58a6b7e9ab240313c7ee490925e61
Alicja <32157812+AlicjaHelenaM@users.noreply.github.com>
2020-04-21 22:16:00 +0200

    basic JS further work

---
+++ 1_BasicJavaScript/word-blanks.js
@@ -0,0 +1,8 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myNoun + " was " + myAdjective + " so I " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
