841c20fbd404b5020c5d3443675fd101246388c3
Alicja <32157812+AlicjaHelenaM@users.noreply.github.com>
2020-04-27 20:38:08 +0200

    basic js

---
+++ 1_BasicJavaScript/local-scope-and-functions.js
@@ -0,0 +1,14 @@
+function myLocalScope() {
+  'use strict';
+
+  // Only change code below this line
+  var myVar = "foo";
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+//console.log(myVar);
+
+// Now remove the console log line to pass the test
---
+++ 1_BasicJavaScript/shopping-list.js
@@ -0,0 +1,7 @@
+var myList = [
+  ["Chocolate Bar", 15],
+  ["White Chocolate Bar", 16],
+  ["Dark Chocolate Bar", 17],
+  ["Salty Carmel Chocolate Bar", 25],
+  ["super-sweet Chocolate Bar", 10],
+  ];
